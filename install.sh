4b167757345a1924ee0e19c5833053c9147bb8cd
GiovsTechs <86326499+GiovsTech@users.noreply.github.com>
2024-04-08 14:23:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [ "$input" == 'y' ]; then
 #         fi
     fi
     if [ "$PM" == "ns" ]; then
-        echo "Supported package managers are APT."
+        echo "Supported package managers are APT and PACMAN(now it isn't)."
         echo "Yours doesn't seem to be supported yet."
         exit
     fi
